55755d9cb2f8b8856f6e067939dff9460ce1bddc
Nima Sherpa <nimadorji01@gmail.com>
2023-07-19 13:58:00 -0400

    Sequelize basic lab completed

--- seed.js
+++ seed.js
@@ -1,6 +1,7 @@
 // This is our seed file. We'll import our Sequelize models from
 // ./database/index and create some dummy data in the database. Seeding a
 // database is very useful for development.
+
 const { blue, cyan, green, red } = require("chalk");
 const { db, Plant } = require("./database");
 
@@ -9,10 +10,18 @@ async function seed() {
     console.log(cyan("📡 Connecting to the database..."));
     // Connect to the database
 
+    await db.sync({force: true});
+
     console.log(blue("🌱 Seeding the database..."));
 
     // Seed the database
+    const cauliflower = await Plant.create({name: "Cauliflower"});
+    console.log("cauliflower instance >>>>", cauliflower);
+    console.log("cauliflower name >>>>", cauliflower.name);
 
+    await cauliflower.update({name: "Pale Broccoli"});
+    console.log("updated cauliflower name >>>>", cauliflower.name);
+    
     // Close the database connection
     await db.close();
 
